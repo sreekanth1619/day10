0af803329ed7d916b99c665b2e8e48b6df38638f
sreekanth1619 <sreekanthsree1619@gmail.com>
2021-08-25 13:24:14 +0530

    sorting the results in decending order

---
+++ uc9.sh
@@ -0,0 +1,35 @@
+#! /bin/bash -x
+
+echo -n:"enter the value";
+read a
+echo -n:"enter the value";
+read b
+echo -n:"enter the value";
+read c
+
+
+     exp1=$(( $a + $b * $c ))
+  echo "$exp1"
+     exp2=$(( $a * $b + $c ))
+  echo "$exp2"
+     exp3=$(( $a % $b + $c ))
+   echo "$exp3"
+
+
+declare -A expressions
+
+expressions[key1]="$exp1"
+expressions[key2]="$exp2"
+expressions[key3]="$exp3"
+
+array=($exp1 $exp2 $exp3)
+
+echo "${expressions[@]}"
+
+echo "$array"
+
+sorted=($(sort >>>"{$array[@]}"));
+   echo "${sorted[@]}"
+
+
+
